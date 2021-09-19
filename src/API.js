27ee473eceb82578f37eb63f56ad6ce0c1846b98
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-09-19 01:10:29 -0300

    Endpoints added and useFetch create

---
+++ src/API.js
@@ -0,0 +1,21 @@
+export const API_URL = 'http://ddragon.leagueoflegends.com/cdn/';
+
+export function SEARCH_IN_VERSION(version) {
+  return {
+    url: API_URL + `${version}/data/pt_BR/champion.json`,
+    options: {
+      method: 'GET',
+      cache: 'no-store',
+    },
+  };
+}
+
+export function SEARCH_CHAMPION(version, champion) {
+  return {
+    url: API_URL + `${version}/data/pt_BR/champion/${champion}.json`,
+    options: {
+      method: 'GET',
+      cache: 'no-store',
+    },
+  };
+}
--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -2,39 +2,16 @@ import React from 'react';
 import Header from '../../Components/Header';
 import Frame from '../../Components/Frame';
 
+import { SEARCH_IN_VERSION } from '../../API';
 import { Wrapper } from './styled';
 
 const Home = () => {
+  React.useEffect(() => {}, []);
+
   return (
     <section>
       <Header />
       <Wrapper>
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
-        <Frame />
         <Frame />
       </Wrapper>
     </section>

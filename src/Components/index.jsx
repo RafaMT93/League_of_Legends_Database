cdf818927e5b23ab181461ad7ccf3657b411d010
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-09-23 01:01:32 -0300

    Added export default for Image

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,3 +1,4 @@
 export { default as Header } from './Header/index';
 export { default as Frame } from './Frame/index';
+export { dafault as Image } from './Image/index';
 export { default as Error } from './Error/index';

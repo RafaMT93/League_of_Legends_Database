50c48fc07fd7dc2240fc2e1787c20f32f8adf236
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-09-23 01:00:51 -0300

    Added propTypes

--- src/Components/Image/index.jsx
+++ src/Components/Image/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Image = ({ src, alt, title }) => {
-  return <img src={src} alt={alt} title={title} />;
+const Image = ({ src, alt, title, height, width }) => {
+  return (
+    <img
+      src={src}
+      style={{ width: `${width}px`, height: `${height}px` }}
+      alt={alt}
+      title={title}
+    />
+  );
+};
+
+Image.defaultProps = {
+  width: 64,
+  height: 64,
+};
+
+Image.propTypes = {
+  width: PropTypes.number,
 };
 
 export default Image;
